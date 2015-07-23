b61cc5760d3d733b1382eebb1dc09ab696a8cc7b
Dionysios Barmpoutis <barmpoutis@gmail.com>
2015-07-22 21:25:43 -0400

    BasicDataTypes

---
+++ BasicDataTypes.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(){
+
+    int i; long l; long long ll; char c; float f; double d;
+    scanf("%d %ld %lld %c %f %lf\n", &i, &l, &ll, &c, &f, &d);
+    printf("%d\n%ld\n%lld\n%c\n%f\n%lf\n", i, l, ll, c, f, d);
+
+    return 0;
+}
