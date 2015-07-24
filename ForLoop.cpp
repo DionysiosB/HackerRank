d8f9d711be8d0a32672fb886f41d30df4d5396cb
Dionysios Barmpoutis <barmpoutis@gmail.com>
2015-07-24 06:24:14 -0400

    ForLoop

---
+++ ForLoop.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+
+int main() {
+    int a, b; scanf("%d\n%d", &a, &b);
+    for(int n = a; n <= b; n++){
+        if(n == 1){puts("one");}
+        else if(n == 2){puts("two");}
+        else if(n == 3){puts("three");}
+        else if(n == 4){puts("four");}
+        else if(n == 5){puts("five");}
+        else if(n == 6){puts("six");}
+        else if(n == 7){puts("seven");}
+        else if(n == 8){puts("eight");}
+        else if(n == 9){puts("nine");}
+        else if(n > 9){
+            if(n % 2){puts("odd");}
+            else{puts("even");}
+        }
+    }
+    
+    return 0;
+}
