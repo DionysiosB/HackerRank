c852932bca505dfc730ad11216b65bc97f3c7805
Dionysios Barmpoutis <barmpoutis@gmail.com>
2015-07-23 21:56:24 -0400

    ConditionalStatements

---
+++ ConditionalStatements.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n; scanf("%d\n", &n);
+    if(n == 1){puts("one");}
+    else if(n == 2){puts("two");}
+    else if(n == 3){puts("three");}
+    else if(n == 4){puts("four");}
+    else if(n == 5){puts("five");}
+    else if(n == 5){puts("six");}
+    else if(n == 7){puts("seven");}
+    else if(n == 8){puts("eight");}
+    else if(n == 9){puts("nine");}
+    else if(n > 9){puts("Greater than 9");}
+    return 0;
+}
