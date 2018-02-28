d87103f5c3d256eeb578cb0376d4c8abf231fb7a
Dionysios Barmpoutis <barmpoutis@gmail.com>
2018-02-28 06:36:07 -0500

    LeftRotation

---
+++ LeftRotation.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <vector>
+
+int main(){
+
+    long n, d; scanf("%ld %ld\n", &n, &d);
+    std::vector<long> a(n);
+    for(long p = 0; p < n; p++){scanf("%ld", &a[p]);}
+    for(long p = 0; p < n; p++){printf("%ld ", a[(p + d) % n]);}
+    puts("");
+
+    return 0;
+}
