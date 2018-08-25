310ef2ff3f84272a774b87a594f1013d2aa1c964
Dionysios Barmpoutis <barmpoutis@gmail.com>
2018-08-25 07:32:48 -0400

    PowersGame

---
+++ PowersGame.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main() {
+    
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        puts((n % 8) ? "First" : "Second");
+    }
+    
+    return 0;
+}
