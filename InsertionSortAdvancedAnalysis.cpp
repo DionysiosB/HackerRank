3ac752478c530fc0222c19c7396740c27f8171f3
Dionysios Barmpoutis <barmpoutis@gmail.com>
2018-08-27 06:11:29 -0400

    InsertionSortAdvancedAnalysis

---
+++ InsertionSortAdvancedAnalysis.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <set>
+#include <algorithm>
+#include <iterator>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        std::multiset<long> s;
+        long cnt(0);
+        while(n--){
+            long x; scanf("%ld", &x);
+            long diff = std::distance(std::upper_bound(s.begin(), s.end(), x), s.end());
+            cnt += diff;
+            s.insert(x);
+        }
+
+        printf("%ld\n", cnt);
+    }
+
+    return 0;
+}
