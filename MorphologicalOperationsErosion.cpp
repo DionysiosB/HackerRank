84458dc066f33351f924017d5475c35a88b3a484
Dionysios Barmpoutis <barmpoutis@gmail.com>
2018-10-04 04:10:00 -0400

    MorphologicalOperationsErosion

---
+++ MorphologicalOperationsErosion.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    
+    std::vector<std::string> m;
+    m.push_back("0000000000");
+    m.push_back("0111111100");
+    m.push_back("0000111100"); 
+    m.push_back("0000111100");
+    m.push_back("0001111100");
+    m.push_back("0000111100");
+    m.push_back("0001100000");
+    m.push_back("0000000000");
+    m.push_back("0000000000");
+    
+    
+    std::vector<std::vector<long> > f(9, std::vector<long>(10, 1));
+    for(long row = 0; row < m.size(); row++){
+        for(long col = 0; col < m[0].size(); col++){
+            if(row == 0 || col == 0){f[row][col] = 0; continue;}
+            for(long dr = -1; dr <= 1; dr++){
+                if(row + dr < 0 || row + dr >= m.size()){continue;}
+                for(long dc = -1; dc <= 1; dc++){
+                    if(col + dc < 0 || col + dc >= m[0].size()){continue;}
+                    if(m[row + dr][col + dc] == '0'){f[row][col] = 0;}
+                }
+            }
+        }
+    }
+    
+    
+    long cnt(0);
+    for(long row = 0; row < m.size(); row++){for(long col = 0; col < m.size(); col++){cnt += f[row][col];}}
+    
+    printf("%ld\n", cnt);
+    
+    return 0;
+}
